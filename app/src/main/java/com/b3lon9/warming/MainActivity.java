5dfb159937c24aa74034678836a2732a661e45fe
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2022-12-05 23:39:20 +0900

    MainFragment Default in MainActivty Container
    굉장히 오랜만이다.
    안드로이드 상급의 실력을 갖고 있다고 자부했지만,
    머리 속에 디자인들을 내 마음껏 만들었었는데
    
    AI를 하는동안 그 감을 다 잊어버렸다...
    Fragment를 Activity에 넣는거 조차 어렵게 느껴진다...
    
    새로움이 좋긴 하지만... 당황스럽다.
    
    [문제이해]
    Fragment에서 attachToRoot를 true로 설정해주었다.
    false로 설정해야 replace가 가능한 것 같다.

--- app/src/main/java/com/b3lon9/warming/MainActivity.java
+++ app/src/main/java/com/b3lon9/warming/MainActivity.java
@@ -1,14 +1,38 @@
 package com.b3lon9.warming;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.b3lon9.warming.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+    MainFragment mainFragment;
+
+    Button mainBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainFragment = new MainFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.remove(mainFragment);
+        ft.replace(R.id.main_fragment, mainFragment);
+        ft.commit();
+
+        mainBtn = findViewById(R.id.main_button);
+        mainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("[debug]", "Button Test, Long Time No See Android");
+
+
+            }
+        });
     }
 }
