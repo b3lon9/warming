97dd1fe404074d86557f041bd3a88302a5773f75
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2022-12-18 23:39:39 +0900

    canvas - 도형 그리기
    점선그리기 : DashPathEffect
    텍스트 입력 drawText
    테두리 점선 입력

--- Demo8/app/src/main/java/com/b3lon9/demo8/Custom.java
+++ Demo8/app/src/main/java/com/b3lon9/demo8/Custom.java
@@ -3,6 +3,7 @@ package com.b3lon9.demo8;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.view.View;
 
@@ -21,9 +22,43 @@ public class Custom extends View {
 
         paint = new Paint();
         // color 지정
+        paint.setStyle(Paint.Style.FILL);   // 기본값
         paint.setColor(Color.GREEN);
 
         // 원 그리기 cy:원의 중심점, radius:반지름
-        canvas.drawCircle(500, 500, 300, paint);
+        canvas.drawCircle(100, 100, 100, paint);
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5.0F);
+        paint.setColor(Color.MAGENTA);
+
+        canvas.drawCircle(100, 100, 100, paint);
+
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(3.0F);
+        paint.setColor(Color.RED);
+        canvas.drawCircle(360, 100, 100, paint);
+
+        // 사각형 그리기
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.BLUE);
+
+        paint.setARGB(128, 255, 0, 0);
+        canvas.drawRect(10, 210, 200,410, paint);
+
+        // 점선으로 바꾸기
+        DashPathEffect dashPathEffect = new DashPathEffect(new float[]{5, 5}, 1);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(10.0F);
+        paint.setPathEffect(dashPathEffect);
+        paint.setColor(Color.BLUE);
+        canvas.drawRect(220, 210, 400, 410, paint);
+
+        // 텍스트
+        paint.setStyle(Paint.Style.FILL);
+        paint.setTextSize(30);
+        String message = "테스트할 문자 입력";
+        canvas.drawText(message, 10, 500, paint);
     }
 }
