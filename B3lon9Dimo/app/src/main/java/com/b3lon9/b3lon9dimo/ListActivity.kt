91d659eb726132518f6f862402919cce22660255
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-01-05 22:34:35 +0900

    메모 앱 레이아웃 구성
    https://www.youtube.com/watch?v=HMGJMwwlSPQ&list=PLQdnHjXZyYaemV1ievGAfgE-YXp3HddnK&index=2

---
+++ B3lon9Dimo/app/src/main/java/com/b3lon9/b3lon9dimo/ListActivity.kt
@@ -0,0 +1,44 @@
+package com.b3lon9.b3lon9dimo
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.b3lon9.b3lon9dimo.databinding.ActivityListBinding
+
+class ListActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_list)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+
+            val intent = Intent(applicationContext, DetailActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_list)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
